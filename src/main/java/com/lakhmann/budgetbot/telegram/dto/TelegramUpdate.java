19c47af29ee878a4b473abcb17946bd7e5b71e01
Lior Lakhmann <125472222+Jormundgar@users.noreply.github.com>
2026-02-25 16:59:45 +0200

    Revert "fix/buttons"

--- src/main/java/com/lakhmann/budgetbot/telegram/dto/TelegramUpdate.java
+++ src/main/java/com/lakhmann/budgetbot/telegram/dto/TelegramUpdate.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record TelegramUpdate(
         @JsonProperty("message") Message message
 ) {
-    public record Message(Chat chat, String text, @JsonProperty("web_app_data") WebAppData webAppData) {}
+    public record Message(Chat chat, String text) {}
     public record Chat(Long id) {}
-    public record WebAppData(String data, @JsonProperty("button_text") String buttonText) {}
 }
