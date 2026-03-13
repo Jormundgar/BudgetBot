96509d9fe01f7f31a59a4c90e7e5261cf5cca671
Lior Lakhmann <liorlakhmann@gmail.com>
2026-03-13 10:46:01 +0200

    Disable internal scheduling and remove Daily retries

--- src/main/java/com/lakhmann/budgetbot/config/SchedulingConfig.java
+++ src/main/java/com/lakhmann/budgetbot/config/SchedulingConfig.java
@@ -1,9 +1,11 @@
 package com.lakhmann.budgetbot.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@ConditionalOnProperty(value = "jobs.scheduling.enabled", havingValue = "true")
 @EnableScheduling
 public class SchedulingConfig {
 }
